0e9e639a17d2deebbc0e6df073a9de0bb24c04a9
AndreaHegyi <andrea.hegyi99@e-uvt.ro>
2022-01-09 22:00:20 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {StudentComponent} from "./student/student.component";
 import {SelectScreenComponent} from "./select-screen/select-screen.component";
 import {StudentinterComponent} from './studentinter/studentinter.component';
 import {StudentregisteredComponent} from "./studentregistered/studentregistered.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
   {path: 'teacher', component: TeacherComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'select-screen', component: SelectScreenComponent},
   {path: 'studentinter', component: StudentinterComponent},
   {path: 'studentregistered', component: StudentregisteredComponent},
+  {path: 'home-page', component: HomePageComponent},
   {path: '', redirectTo: '/select-screen', pathMatch: 'full'}
 ];
 
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Classroom} from '../classroom';
+import {User} from '../user';
+import {HttpErrorResponse} from '@angular/common/http';
+import {UserService} from '../user.service';
+import {Subject} from '../subject';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +14,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  public users: User[];
+  public classrooms: Classroom[];
+  public subjects: Subject[];
+  public isStudent: boolean;
 
-  ngOnInit(): void {
+
+  constructor(private userService: UserService, private router: Router) {
+    this.isStudent = false;
+  }
+
+  ngOnInit(): void{
+    this.getUsers();
+  }
+
+  public addAccount(userForm: NgForm): void{
+    document.getElementById('closeUserForm').click();
+    userForm.value.isStudent = this.isStudent;
+    console.log(userForm.value);
+    this.userService.addUser(userForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        userForm.reset();
+        if (this.isStudent) {
+          this.router.navigate(['/studentinter']);
+        }
+        else { this.router.navigate(['/teacher']); }
+
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+        console.log(error);
+      }
+    );
+  }
+
+
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (userResponse: User[]) => {
+        this.users = userResponse;
+        console.log(userResponse);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 
 }
